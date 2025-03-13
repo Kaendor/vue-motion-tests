c1b6bec511c5c05fb13aa097b49a2374c2a751ad
Dimitri Teboul <dimitri.teboul@indy.fr>
2025-03-13 17:58:03 +0100

    use Motion component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 // import { animate } from 'motion';
-import { animate, Motion } from 'motion-v';
+import { animate, Motion, AnimatePresence } from 'motion-v';
 import { ref } from 'vue';
 
 const display = ref("10");
@@ -16,22 +16,18 @@ animate(10, 100, {
 </script>
 
 <template>
-  <Motion 
-    class="box" 
-    :transition="{
-      duration: 1,
-    }"
-    :hover="{
-      scale: 2.5,
-    }"
-    :initial="{
-      rotate: 180,
-      scale: 1,
-    }" 
-    :animate="{
-      rotate: 0,
-      scale: 2,
-    }">{{display}}</Motion>
+  <AnimatePresence>
+    <Motion
+      class="box"
+      :initial="{ scale: 0 }"
+      :animate="{ rotate: 180, scale: 1 }"
+      :exit="{ rotate: 0, scale: 0 }"
+      :press="{ scale: 0.8 }"
+      :hover="{ scale: 1.2 }"
+    >
+
+    </Motion>
+  </AnimatePresence>
 </template>
 
  <style>
